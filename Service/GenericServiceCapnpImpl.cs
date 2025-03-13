8c3bb0b81716130c95f9f6d9ba3c82c9a3a7b7b7
Vincent Dlugosch <vincent.dlugosch@zalf.de>
2025-03-13 18:09:37 +0100

    chore: move to directory to match namespace

--- Service/GenericServiceCapnpImpl.cs
+++ Service/GenericServiceCapnpImpl.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Mas.Infrastructure.Service
+namespace Mas.Infrastructure.Common.Service
 {
     public class Admin : Mas.Schema.Service.IAdmin
     {
